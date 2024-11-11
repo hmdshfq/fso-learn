45edd1ddcb8a4553afd629d13d8dcb26943ad457
Hammad <hmdshfq@outlook.com>
2024-11-11 13:32:26 +0100

    feat: add increase, decrease, and reset buttons

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,11 +3,26 @@ import React from "react";
 const App = () => {
     const [counter, setCounter] = React.useState(0);
 
-    setTimeout(() => setCounter(counter + 1), 1000);
+    const increaseByOne = () => {
+        setCounter(counter + 1);
+    };
 
-    console.log("rendering...", counter);
+    const decreaseByOne = () => {
+        setCounter(counter - 1);
+    };
 
-    return <div>{counter}</div>;
+    const setToZero = () => {
+        setCounter(0);
+    };
+
+    return (
+        <div>
+            <div>{counter}</div>
+            <button onClick={decreaseByOne}>-</button>
+            <button onClick={setToZero}>0</button>
+            <button onClick={increaseByOne}>+</button>
+        </div>
+    );
 };
 
 export default App;
