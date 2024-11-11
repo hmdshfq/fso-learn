1ed867f2943d49d1dafa0ea5f746b5c915782a4f
Hammad <hmdshfq@outlook.com>
2024-11-11 13:10:30 +0100

    feat: greetings app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = ({ name, age }) => {
+    const bornYear = () => new Date().getFullYear() - age;
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old
+            </p>
+            <p>So you were probably born in {bornYear()}</p>
+        </div>
+    );
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const name = "Peter";
+    const age = 10;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="Maya" age={26+10} />
+            <Hello name={name} age={age} />
+        </div>
+    )
+};
 
-export default App
+export default App;
