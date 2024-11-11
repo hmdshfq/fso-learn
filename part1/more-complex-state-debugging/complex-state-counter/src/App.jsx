efb9e734855c1297e17e7487979fc477e3c084aa
Hammad <hmdshfq@outlook.com>
2024-11-11 14:07:09 +0100

    feat: complex state — add left, right, and allClicks states

--- part1/more-complex-state-debugging/complex-state-counter/src/App.jsx
+++ part1/more-complex-state-debugging/complex-state-counter/src/App.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
-const App = () => {};
+const App = () => {
+    const [left, setLeft] = React.useState(0);
+    const [right, setRight] = React.useState(0);
+    const [allClicks, setAllClicks] = React.useState([]);
+
+    const handleLeftClick = () => {
+        setAllClicks([...allClicks, "L"]);
+        setLeft(left + 1);
+    };
+
+    const handleRightClick = () => {
+        setAllClicks([...allClicks, "R"]);
+        setRight(right + 1);
+    };
+
+    return (
+        <div>
+            {left}
+            <button onClick={handleLeftClick}>Left</button>
+            <button onClick={handleRightClick}>Right</button>
+            {right}
+            <p>{allClicks.join(" ")}</p>
+        </div>
+    );
+};
 
 export default App;
