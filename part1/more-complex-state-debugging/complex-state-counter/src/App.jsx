ceabc28c5e0e835aafc906540e9bda4b8232052a
Hammad <hmdshfq@outlook.com>
2024-11-11 15:58:55 +0100

    feat: complex state counter — History and Button components

--- part1/more-complex-state-debugging/complex-state-counter/src/App.jsx
+++ part1/more-complex-state-debugging/complex-state-counter/src/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+const History = ({ allClicks }) => {
+    if (allClicks.length === 0) {
+        return <p>This app is used by pressing the buttons</p>;
+    }
+    return <p>Button press history: {allClicks.join(" ")}</p>;
+};
+
+const Button = ({ onClick, text }) => {
+    return <button onClick={onClick}>{text}</button>;
+};
+
 const App = () => {
     const [left, setLeft] = React.useState(0);
     const [right, setRight] = React.useState(0);
@@ -7,14 +18,14 @@ const App = () => {
     const [total, setTotal] = React.useState(0);
 
     const handleLeftClick = () => {
-        const nextNumber = left + 1
+        const nextNumber = left + 1;
         setAllClicks([...allClicks, "L"]);
         setLeft(nextNumber);
         setTotal(nextNumber + right);
     };
 
     const handleRightClick = () => {
-        const nextNumber = right + 1
+        const nextNumber = right + 1;
         setAllClicks([...allClicks, "R"]);
         setRight(nextNumber);
         setTotal(left + nextNumber);
@@ -23,11 +34,11 @@ const App = () => {
     return (
         <div>
             {left}
-            <button onClick={handleLeftClick}>Left</button>
-            <button onClick={handleRightClick}>Right</button>
+            <Button onClick={handleLeftClick} text="Left" />
+            <Button onClick={handleRightClick} text="Right" />
             {right}
-            <p>{allClicks.join(" ")}</p>
             <p>Total {total}</p>
+            <History allClicks={allClicks} />
         </div>
     );
 };
