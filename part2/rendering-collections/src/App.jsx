a29118f07472bb67c21a9ffd75e20d1e27019db3
Hammad <hmdshfq@outlook.com>
2024-11-19 21:53:57 +0100

    feat: part 2d — promises and errors

--- part2/rendering-collections/src/App.jsx
+++ part2/rendering-collections/src/App.jsx
@@ -30,7 +30,13 @@ const App = () => {
                 setNotes(
                     notes.map((note) => (note.id === id ? returnedNotes : note))
                 );
-        });
+            })
+            .catch(error => {
+                alert(
+                    `the note '${note.content}' was already deleted from server`
+                )
+                setNotes(notes.filter(note => note.id !== id))
+            })
     };
 
     useEffect(() => {
