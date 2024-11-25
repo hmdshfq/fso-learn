4854174ddae2556118ff046d2854dbc330864a16
Hammad <hmdshfq@outlook.com>
2024-11-25 19:52:19 +0100

    feat: add Express server and API endpoint for notes; update package.json and .gitignore

--- part3/3a/index.js
+++ part3/3a/index.js
@@ -1,7 +1,8 @@
-const http = require("http");
+const express = require('express')
 const PORT = 3001;
+const app = express();
 
-let notes = [
+const notes = [
     {
         id: "1",
         content: "HTML is easy",
@@ -19,10 +20,13 @@ let notes = [
     },
 ];
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(notes));
-});
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
 
 app.listen(PORT);
 console.log(`Server is running at port ${PORT}`);
