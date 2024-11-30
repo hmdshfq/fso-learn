bc0c27c2e8f048d7b58edf6ea6b938863dd4c843
Hammad <hmdshfq@outlook.com>
2024-11-30 01:08:05 +0100

    feat: update API base URL, serve static files from 'dist', and update script references in HTML

--- part3/index.js
+++ part3/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 // Middleware to parse the body of the request
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
     console.log("Method: ", request.method);
